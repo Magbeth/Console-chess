456e1f6c2e9958648bfb6247a255f61ae021d569
Magbeth <magbethlmc@gmail.com>
2019-01-20 22:20:15 +0300

    Queen logic added

--- src/com/company/pieces/King.java
+++ src/com/company/pieces/King.java
@@ -3,15 +3,22 @@ package com.company.pieces;
 import com.company.Board;
 
 public class King extends Pieces {
-    public King() {
-    }
 
+
+    //check for move is possible
+    @Override
     public boolean isMoveLegal(Pieces[][] pieces, int x, int y, int i, int j) {
-        return true;
+        hasObstacle = false;
+        //check for ally on destination point
+        if(isDestinationAlly(pieces[x][y], pieces[i][j])) return false;
+        else return false;
     }
 
     public King(Board.Color color) {
         this.color = color;
+    }
+
+    public King() {
     }
 
     @Override
--- src/com/company/pieces/Queen.java
+++ src/com/company/pieces/Queen.java
@@ -3,14 +3,33 @@ package com.company.pieces;
 import com.company.Board;
 
 public class Queen extends Pieces {
-    public Queen() {
-    }
 
+    //check for move is possible
+    @Override
     public boolean isMoveLegal(Pieces[][] pieces, int x, int y, int i, int j) {
-        return true;
+        hasObstacle = false;
+        //check for ally on destination point
+        if(isDestinationAlly(pieces[x][y], pieces[i][j])) return false;
+
+        //check for obstacle on path. Possible moves are Rook-like or Bishop-like. Using its methods.
+        //Bishop-like
+        if (Math.abs(i - x) == Math.abs(j - y) ) {
+            Pieces bishop = new Bishop();
+            return bishop.isMoveLegal(pieces, x, y, i, j);
+        }
+        //Rook-like
+        if (i - x == 0 || j - y == 0) {
+            Pieces rook = new Rook();
+            return rook.isMoveLegal(pieces, x, y, i, j);
+        }
+
+        else return false;
     }
     public Queen(Board.Color color) {
         this.color = color;
+    }
+
+    public Queen() {
     }
 
     @Override
