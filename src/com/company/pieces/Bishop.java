5b42a1e8d9be9b158f52ae26814921dd0bfcc31e
Magbeth <magbethlmc@gmail.com>
2019-01-21 12:59:40 +0300

    Bishop moves fixed

--- src/com/company/pieces/Bishop.java
+++ src/com/company/pieces/Bishop.java
@@ -11,15 +11,42 @@ public class Bishop extends Pieces {
         //check for ally on destination point
         if(isDestinationAlly(pieces[x][y], pieces[i][j])) return false;
         //check for obstacle on path
-        int m = 1;
-        for (int k = Math.min(x, i)+1; k < Math.max(x, i); k++) {
-            int l = Math.max(y, j);
-            if (pieces[k][l-m] != null) {
-                hasObstacle = true;
-                break;
+//        int m = 1;
+//        for (int k = Math.min(x, i)+1; k < Math.max(x, i); k++) {
+//            int l = Math.max(y, j); // l = 5
+//            if (pieces[k][j-m] != null) { //02 35 -- x = {1,2} 14 23  | 13 24
+//                hasObstacle = true;
+//                break;
+//            }
+//            m++;
+//        }
+        if ( (x > i && y > j) || (x < i && j > y)) {
+            for (int k = 1; k < Math.abs(i - x); k++) {
+                int a = Math.max(x, i);
+                int b = Math.max(y, j);
+                if (pieces[a - k][b - k] != null) {
+                    hasObstacle = true;
+                    break;
+                }
             }
-            m++;
         }
+        else if (x > i && j > y) {
+            for (int k = 1; k < Math.abs(i - x); k++) {
+                if (pieces[i + k][j - k] != null) {
+                    hasObstacle = true;
+                    break;
+                }
+            }
+        }
+        else if (x < i && j < y) {
+            for (int k = 1; k < Math.abs(i - x); k++) {
+                if (pieces[i - k][j + k] != null) {
+                    hasObstacle = true;
+                    break;
+                }
+            }
+        }
+        //TODO Check moves
         //possible moves
         if (Math.abs(i - x) == Math.abs(j - y) && !hasObstacle) return true;
         else return false;
